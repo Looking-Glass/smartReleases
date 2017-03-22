3e538913a2943fb4739bca9e85d7a61e3c17d310
enjrolas <alex@lookingglassfactory.com>
2017-03-22 08:34:26 -0400

    added working example of lkg website markup

--- sample-library-page/standalone-github.js
+++ sample-library-page/standalone-github.js
@@ -1,22 +1,14 @@
-function updateProjects()
-{
-    //loops through all the "project" divs in the html and updates the links to the latest github release
-    $("div.project")
-	.each(function()
-	      { 
-		  updateLinks($(this));
-	      });
-}
-	
-//takes in a jquery div element and updates the download links inside
-//note -- function will do stupid stuff if there is no url attribute in the div
-function updateLinks(div) {
-    var url=div.attr("url");
+$(function(){
+	updateProjectLinks();
+    });
+
+function updateProjectLinks() {    
+    var url=$("div.s-single--library:first").attr("url");
     var releaseUrl=url+"releases/latest/";
     //first, set all links to project URL, so we have something in case the API request fails
-    div.find("a#mac").attr("href", releaseUrl);
-    div.find("a#win").attr("href", releaseUrl);
-    div.find("a#src").attr("href", url);
+    $("a#mac").attr("href", releaseUrl);
+    $("a#win").attr("href", releaseUrl);
+    $("a#src").attr("href", url);
     
     //next, build the API URL for this github project
     var apiUrl=url.replace("https://github.com/", "https://api.github.com/repos/");
@@ -43,8 +35,8 @@ function updateLinks(div) {
 		winUrl=release.html_url;
 
 	    //finally, update the links in the div
-	    div.find("a#mac").attr("href", macUrl);
-	    div.find("a#win").attr("href", winUrl);
-	    div.find("a#src").attr("href", sourceUrl);
+	    $("a#mac").attr("href", macUrl);
+	    $("a#win").attr("href", winUrl);
+	    $("a#src").attr("href", sourceUrl);
         });
 }
